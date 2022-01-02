07ab4dd2fb3d6ed04efa54c0f7ed5bc18c7eb0a8
razerx100 <saikatmandal8@gmail.com>
2022-01-02 17:50:00 +0600

    added some nodiscards

--- includes/CRSVector.hpp
+++ includes/CRSVector.hpp
@@ -43,16 +43,19 @@ namespace Ceres {
 		return _mm_div_ps(vec1, vec2);
 	}
 
+	[[nodiscard]]
 	inline Vector LoadFloat32_2(const Float32_2& f32_2) noexcept {
 		return _mm_castpd_ps(_mm_load_sd(reinterpret_cast<const double*>(&f32_2.x)));
 	}
 
+	[[nodiscard]]
 	inline Vector LoadFloat32_3(const Float32_3& f32_3) noexcept {
 		__m128 xy = _mm_castpd_ps(_mm_load_sd(reinterpret_cast<const double*>(&f32_3.x)));
 		__m128 z = _mm_load_ss(&f32_3.z);
 		return _mm_movehl_ps(xy, z);
 	}
 
+	[[nodiscard]]
 	inline Vector LoadFloat32_4(const Float32_4& f32_4) noexcept {
 		return _mm_castpd_ps(_mm_load_pd(reinterpret_cast<const double*>(&f32_4.x)));
 	}
